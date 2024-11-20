9e9d8f535758c4e87e7673dd3cab898ddf66c0c2
Andres G <AndresG_99_@hotmail.com>
2024-11-19 22:09:46 -0500

    Primer commit :)

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+
+
+<template>
+  <div class="card flex justify-center">
+      <DatePicker v-model="date" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import DatePicker from 'primevue/datepicker';
+
+const date = ref();
+</script>
